c02c7469a9715ab39bb932e4e614173082745cce
allentran <realallentran@gmail.com>
2014-10-02 11:14:06 -0700

    made gitignore work

---
+++ miner/HashtagMachine.py
@@ -0,0 +1,16 @@
+import requests
+import numpy as np
+import pandas as pd
+
+class Hashtagerator:
+
+    def __init__(self,keyword='ebola'):
+
+        self.keyword = keyword
+
+    def get_images(self):
+
+
+    def juice_image(self,img):
+
+
